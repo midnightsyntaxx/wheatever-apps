dcf7d0709c908ac11df7f8b3415ddfdf7ffc2863
Newbie-Programmer <218341976+midnightsyntaxx@users.noreply.github.com>
2026-05-01 13:45:56 +0800

    weather-apps

--- api/get-weather.js
+++ api/get-weather.js
@@ -9,6 +9,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({error: "Gagal mengambil data cua"});
+    res.status(500).json({error: "Gagal mengambil data cuaca"});
   }
 }
